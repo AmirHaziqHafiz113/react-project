3ede7908549c9e6b284dcee4768a67d164580d7a
FinnD4C <64826923+AmirHaziqHafiz113@users.noreply.github.com>
2023-06-11 16:59:48 +0800

    Added modal

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,19 @@
 import React from 'react';
+
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        {
-        id: 'u1',
-        name: 'Max Schwarz',
-        image:
-            'https://images.unsplash.com/photo-1587613868258-4e8e9d4a7a1a?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        places: 3
-        }
-    ];
-    
-    return <UsersList items={USERS} />;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
